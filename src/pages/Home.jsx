fb941a9b4c57c09797ebd2f5fc4a522d1ded3638
Dipak Majumdar <rahulmajumdar400@gmail.com>
2025-10-25 01:02:37 +0530

    hero content updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,15 +5,9 @@ import Projects from '../components/Projects';
 import Skills from '../components/Skills';
 
 function Home({ home, projects, skills }) {
-  console.log("home", home.skills);
-  const title = "Dipak Majumdar <br /> <span>Software Engineer</span>";
-  const description = "I'm a software engineer with a passion for building web applications.";
-  const button = "Contact Me";
-  const status = "Available for work";
-
   return (
     <>
-      <Hero title={title} description={description} button={button} status={status} />
+      <Hero title={home.hero.title} description={home.hero.description} button={home.hero.button} status={home.hero.status} />
       <Projects section={home.projects} t={projects} limit={3} />
       <Skills section={home.skills} t={skills} />
     </>
