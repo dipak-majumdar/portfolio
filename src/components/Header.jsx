64abc4c5af475ee6755988c50f585748e82af0b5
Dipak Majumdar <rahulmajumdar400@gmail.com>
2025-11-07 01:25:03 +0530

    cv download button added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -54,7 +54,7 @@ const Header = ({ t }) => {
                         <span className="logo__name" aria-live="polite">{now.toLocaleTimeString(currentLanguage || 'en-US')}</span>
                     </Link>
                     
-                    <div className="dropdown" onClick={() => setShowLanguageDropdown(!showLanguageDropdown)}>
+                    {/* <div className="dropdown" onClick={() => setShowLanguageDropdown(!showLanguageDropdown)}>
                         <span className="dropdown__label">{currentLanguage}</span>
                         {showLanguageDropdown && (
                             <div className="dropdown__list">
@@ -72,6 +72,15 @@ const Header = ({ t }) => {
                                 ))}
                             </div>
                         )}
+                    </div> */}
+                    {/* <button className='download-btn'>Download CV</button> */}
+                    <div class="download-button" data-tooltip="Size: 20Mb">
+                        <div class="button-wrapper">
+                            <div class="text">Download CV</div>
+                            <span class="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="2em" height="2em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15V3m0 12l-4-4m4 4l4-4M2 17l.621 2.485A2 2 0 0 0 4.561 21h14.878a2 2 0 0 0 1.94-1.515L22 17"></path></svg>
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
